b2b31abccba6e5074ff4cb3bd160192ed66b5439
Sobit Bhattarai <sobitbhattarai111@gmail.com>
2026-03-10 22:55:01 +0545

    phase 2: installed react router dom, setup routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,44 @@
 import React from 'react'
-
-const App = () => {
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
+import Home from "./pages/Home"
+import Services from "./pages/Services"
+import Contact from "./pages/Contact"
+function App() {
   return (
-    <div className='text-red-400'>App</div>
+    <Router>
+      <Routes>
+        <Route path='/' element={ <Home /> } />
+        <Route path='/home' element={<Home />} />
+        <Route path='/services' element={<Services />} />
+        <Route path='/contact' element={<Contact />} />
+      </Routes>
+    </Router>
   )
 }
 
 export default App
+
+// import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+// // Page haru import gareko
+// import Home from './pages/Home';
+// import Services from './pages/Services';
+// import Contact from './pages/Contact';
+
+// function App() {
+//   return (
+//     <Router>
+//       <Routes>
+//         {/* "/" bhanya Home page ho */}
+//         <Route path="/" element={<Home />} />
+        
+//         {/* "/services" bhanya Services page ho */}
+//         <Route path="/services" element={<Services />} />
+        
+//         {/* "/contact" bhanya Contact page ho */}
+//         <Route path="/contact" element={<Contact />} />
+//       </Routes>
+//     </Router>
+//   );
+// }
+
+// export default App;
